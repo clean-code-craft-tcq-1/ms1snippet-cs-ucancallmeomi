9d2023857a09547b4238fe8e9283137c61394e1c
ucancallmeomi <74286342+ucancallmeomi@users.noreply.github.com>
2021-03-25 20:01:04 +0530

    Update SensorValidatorTest.cs

--- SensorValidate.Tests/SensorValidatorTest.cs
+++ SensorValidate.Tests/SensorValidatorTest.cs
@@ -9,15 +9,30 @@ namespace SensorValidate.Tests
     {
         [Fact]
         public void reportsErrorWhenSOCjumps() {
-            Assert.False(SensorValidator.validateSOCreadings(
-                new List<double>{0.0, 0.01, 0.5, 0.51}
+            Assert.True(SensorValidator.validateReadings(
+                new List<double>{0.0, 0.01, 0.5, 0.51},0.05
             ));
         }
         [Fact]
         public void reportsErrorWhenCurrentjumps() {
-            Assert.False(SensorValidator.validateCurrentreadings(
-                new List<double>{0.03, 0.03, 0.03, 0.33}
+            Assert.True(SensorValidator.validateReadings(
+                new List<double>{0.03, 0.03, 0.03, 0.33},0.1
             ));
         }
+        [Fact]
+        public void reportsOkWhenValuesNotEmpty()
+        {
+            Assert.False(SensorValidator.validateReadings(
+                new List<double> { 0.03, 0.03, 0.03, 0.33 }, 0.7
+            ));
+        }
+        [Fact]
+        public void reportsEmptyWhenValuesEmpty()
+        {
+            Assert.True(SensorValidator.validateReadings(
+                new List<double> {}, 0.9
+            ));
+        }
+
     }
 }
